1fc4bbd7918f798b7f5be7b29b2f587d9a34110f
Jeff <j3ffh95@gmail.com>
2023-02-28 10:45:19 -0500

    added pseudocode to the db file

--- db.js
+++ db.js
@@ -1,14 +1,23 @@
-const dotenv = require('dotenv')
-dotenv.config()
-const {MongoClient} = require('mongodb')
+// This is the dotenv package use to create environmental varibles
+const dotenv = require("dotenv");
+dotenv.config();
+// This mongodb package is to connect to the mongodb database
+const { MongoClient } = require("mongodb");
 
-const client = new MongoClient(process.env.CONNECTIONSTRING)
+// we assign the mongoclient to a client variable using the connectionstring given to us
+// by Mongo DB
+const client = new MongoClient(process.env.CONNECTIONSTRING);
 
+// created a start function to call and make the connection - using async/await
+// because we dont know how long is it going to take to connect
 async function start() {
-  await client.connect()
-  module.exports = client
-  const app = require('./app')
-  app.listen(process.env.PORT)
+  await client.connect();
+  // we export from this file the client obj
+  module.exports = client;
+  // we start our server when we first make a connection to MongoDB
+  const app = require("./app");
+  app.listen(process.env.PORT);
 }
 
-start()
+// Start the application
+start();
