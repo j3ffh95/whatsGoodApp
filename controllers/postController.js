d6e7ea7b4c7989e5ec334d717095b3ae591d7ee0
Jeff <j3ffh95@gmail.com>
2023-03-17 15:39:17 -0400

    made a cleanup and validate function

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,3 +1,19 @@
+const Post = require("../models/Post");
+
 exports.viewCreateScreen = function (req, res) {
   res.render("create-post");
+};
+
+exports.create = function (req, res) {
+  // assign post to a new instance of the Post model and passed in the form information
+  let post = new Post(req.body);
+
+  post
+    .create()
+    .then(function () {
+      res.send("new post created");
+    })
+    .catch(function (errors) {
+      res.send(errors);
+    });
 };
