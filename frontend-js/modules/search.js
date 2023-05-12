bfd6e8b8b209d793a23bb2fd11625454d5a2d1c6
Jeff <j3ffh95@gmail.com>
2023-05-11 23:51:45 -0400

    made openOverlay method

--- frontend-js/modules/search.js
+++ frontend-js/modules/search.js
@@ -2,14 +2,19 @@ export default class Search {
   // 1. Select DOM elements, and keep track of any useful data
   constructor() {
     this.headerSearchIcon = document.querySelector(".header-search-icon");
+    this.events();
   }
 
   // 2. Events
   events() {
     this.headerSearchIcon.addEventListener("click", e => {
       e.preventDefault();
+      this.openOverlay();
     });
   }
 
   // 3, Methods
+  openOverlay() {
+    alert("open overlay method just ran.");
+  }
 }
