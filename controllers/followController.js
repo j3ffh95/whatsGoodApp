f9ded28209359a2902b8bdb052b497e523144a62
Jeff <j3ffh95@gmail.com>
2023-06-08 23:06:41 -0400

    Created the addFollow function

--- controllers/followController.js
+++ controllers/followController.js
@@ -1,3 +1,18 @@
 const Follow = require("../models/Follow");
 
-exports.addFollow = function (req, res) {};
+exports.addFollow = function (req, res) {
+  let follow = new Follow(req.params.username, req.visitorId);
+
+  follow
+    .create()
+    .then(() => {
+      req.flash("success", `Successfully followed ${req.params.username}`);
+      res.session.save(() => res.redirect(`/profile/${req.params.username}`));
+    })
+    .catch(errors => {
+      errors.forEach(error => {
+        req.flash("errors", error);
+      });
+      res.session.save(() => res.redirect("/"));
+    });
+};
