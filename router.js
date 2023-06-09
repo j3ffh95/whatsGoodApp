44e593b2fbe214b6e56b2ced76668c4fdce40c7e
Jeff <j3ffh95@gmail.com>
2023-06-08 22:58:52 -0400

    Imported follow Controller to our router

--- router.js
+++ router.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require("./controllers/userController");
 const postController = require("./controllers/postController");
+const followController = require("./controllers/followController");
 
 // User related routes
 router.get("/", userController.home);
