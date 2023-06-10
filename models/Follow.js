45b0cefae40992841d38a9d23c7c453756ace090
Jeff <j3ffh95@gmail.com>
2023-06-09 22:15:24 -0400

    Made the create function for the Follow class instances

--- models/Follow.js
+++ models/Follow.js
@@ -1,3 +1,11 @@
 let Follow = function () {};
 
+// Create function
+Follow.prototype.create = function () {
+  return new Promise((resolve, reject) => {
+    this.cleanUp();
+    this.validate();
+  });
+};
+
 module.exports = Follow;
