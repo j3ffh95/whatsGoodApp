8ebb41f5dbe4769f8dff8695d56ff0f612c68cdd
Jeff <j3ffh95@gmail.com>
2023-06-09 22:18:10 -0400

    Passed in the followedUsername and also the id of the current user that wants to follow

--- models/Follow.js
+++ models/Follow.js
@@ -1,4 +1,8 @@
-let Follow = function () {};
+let Follow = function (followedUsername, authorId) {
+  this.followedUsername = followedUsername;
+  this.authorId = authorId;
+  this.errors = [];
+};
 
 Follow.prototype.cleanUp = function () {};
 
