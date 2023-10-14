4451d92d5bc337b012c2fe0d2bcc7ddfc11d8623
Jeff <j3ffh95@gmail.com>
2023-10-14 02:17:41 -0400

    created HTML to inject in the chat element

--- frontend-js/main.js
+++ frontend-js/main.js
@@ -1,6 +1,12 @@
 import Search from "./modules/search";
-import Search from "./modules/search";
+import Chat from "./modules/chat";
 
+// This if statement checks to see if the header-search-icon exist in the page
 if (document.querySelector(".header-search-icon")) {
   new Search();
+}
+
+// This if statement checks to see if the chat element exist in the page
+if (document.querySelector("#chat-wrapper")) {
+  new Chat();
 }
