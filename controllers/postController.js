f8976c9a26dbd28a92ecadbb14df62b021db2a31
Jeff <j3ffh95@gmail.com>
2024-01-14 17:06:40 -0500

    Change from promises to using async await in the post controller with the edit function

--- controllers/postController.js
+++ controllers/postController.js
@@ -73,10 +73,37 @@ exports.viewEditScreen = async function (req, res) {
   }
 };
 
-exports.edit = function (req, res) {
+exports.edit = async function (req, res) {
   // Making a Post instance with the required information to update it
   // with the form body data, the visitor id and also the id of the current post
   let post = new Post(req.body, req.visitorId, req.params.id);
+
+  // USING ASYNC AWAIT INSTEAD OF PROMISE
+  // try {
+  //   const status = await post.update()
+  //   if (status == "success") {
+  //     // post was updated in db - create a flash success message
+  //     req.flash("success", "Post successfully updated.");
+  //     req.session.save(function () {
+  //       res.redirect(`/post/${req.params.id}/edit`);
+  //     });
+  //   } else {
+  //     // There was validation errors so we are going to iterate through the errors array
+  //     // from the post object and create a flash message for each of them
+  //     post.errors.forEach(function (error) {
+  //       req.flash("errors", error);
+  //     });
+  //     // Manually save the session data and redirect them to the same edit page
+  //     req.session.save(function () {
+  //       res.redirect(`/post/${req.params.id}/edit`);
+  //     });
+  //   }
+  // } catch (errors) {
+  //   req.flash("errors", "You do not have permission to perform that action.");
+  //     req.session.save(function () {
+  //       res.redirect("/");
+  //     });
+  // }
 
   post
     .update()
